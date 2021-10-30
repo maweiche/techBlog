6137fd49573adfa3c897f045423828e30acae9f1
Matt Weichel <maweiche@gmail.com>
2021-10-30 09:55:19 -0700

    created comment model and exported

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,47 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 //create Comment model
+class Comment extends Model {}
 
 //create comment column
-
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+);
 //export comment model
+module.exports = Comment;
