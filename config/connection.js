4e4a2eee4b3b4acad30c178121bbb95b93173b78
Matt Weichel <maweiche@gmail.com>
2021-10-30 10:23:20 -0700

    added connection

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+//import sequelize
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+let sequelize;
+
+//heroku deployment
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    //localhost
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3001
+    });
+}
+
+module.exports = sequelize;
