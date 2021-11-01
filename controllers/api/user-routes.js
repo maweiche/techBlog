80b640b3d69823a301a3c8b4075ce90f60131827
Matt Weichel <maweiche@gmail.com>
2021-10-31 17:46:36 -0700

    added to user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,7 +55,7 @@ router.get('/:id', (req, res) => {
 });
 
 //CREATE new user
-router.post('/', withAuth, (req, res) => {
+router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -100,6 +100,57 @@ router.post('/login', (req, res) => {
     });
 });
 
+//Log-out
+router.post('/logout', withAuth, (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }else{
+        res.status(404).end();
+    }
+});
+
+//update user
+router.put('/:id', withAuth, (req, res) => {
+    User.update(req.body {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData[0]) {
+            res.status(404).json({ message: 'No user found with matching id'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Delete user
+router.delete('/:id', withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with matching id.'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 //export
 module.exports = router;
